7836df2bc6a3516ffdf991751bc3e45c2ac71888
BeArnis <berzins555@gmail.com>
2014-01-03 12:22:32 +0200

    Transferring necessary data from Stack.js to StackSpec.js

--- spec/StackSpec.js
+++ spec/StackSpec.js
@@ -1,5 +1,28 @@
 /*global describe, it, expect, IS, Stack_init, a, p, s, Stack_pop, Stack_is_empty, Stack_push, renum, restack */
 describe("Stack", function () {
+  var a = new Stack_init();
+  var s = new Stack_init();
+  Stack_push(s, 9);
+  var m = Stack_pop(s);
+
+  var num = [1, 2, 3, 4];
+
+  var renum = [4, 3, 2, 1];
+
+  var restack = [];
+
+  var p = 9;
+
+  var i;
+  for (i = 0; i < 4; i++) {
+    Stack_push(a, num[i]);
+  }
+
+  for (i = 0; i < 4; i++) {
+    restack[i] = Stack_pop(a);
+  }
+
+
   it("should be empty after init", function () {
     expect(Stack_init()).toBeDefined;
   });
--- src/Stack.js
+++ src/Stack.js
@@ -21,24 +21,3 @@ function Stack_pop(Stack) {
   }
   return Stack.pop();
 }
-var a = new Stack_init();
-var s = new Stack_init();
-Stack_push(s, 9);
-var m = Stack_pop(s);
-
-var num = [1, 2, 3, 4];
-
-var renum = [4, 3, 2, 1];
-
-var restack = [];
-
-var p = 9;
-
-var i;
-for (i = 0; i < 4; i++) {
- Stack_push(a, num[i]);
-}
-
-for (i = 0; i < 4; i++) {
- restack[i] = Stack_pop(a);
-}
