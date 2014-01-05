153ceda3770ee7ccdad84587fa534eef6ba51e23
BeArnis <berzins555@gmail.com>
2014-01-05 14:50:57 +0200

    Stack is manual now and has two properties

--- src/Stack.js
+++ src/Stack.js
@@ -1,23 +1,35 @@
 function stack_init() {
-  var stack = new Array();
-  return stack;
+  var i = -1;
+  var stack = [];
+  stack.storage = stack;
+  stack.length1 = i;
+  return stack.storage;
 }
 
 function stack_is_empty(stack) {
-  if (stack.length === 0) {
+  if (stack.length1 === -1) {
     return true;
   }
   return false;
 }
 
 function stack_push(stack, item) {
-  stack.push(item);
+  var i = stack.length1;
+  i++;
+  stack[i] = item;
+  stack.storage = stack;
+  stack.length1 = i;
   return stack;
 }
 
 function stack_pop(stack) {
-  if (stack.length === 0) {
+  if (stack.length1 === -1) {
     return "error";
   }
-  return stack.pop();
+  var i = stack.length1;
+  var pop = stack[i];
+  --i;
+  stack.storage = stack;
+  stack.length1 = i;
+  return pop;
 }
